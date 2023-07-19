e2567513a41aa76f7254c185bdd0dbe65912287c
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-07-19 14:24:36 +0200

    Implement tasks and subtasks components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,94 @@
+import { ReactNode } from "react";
+
+type CardProps = {
+  children: ReactNode;
+};
+
+type SubtaskProps = {
+  name: string;
+  duration: number;
+};
+
+type TaskProps = {
+  name: string;
+  duration: number;
+  subtasks: SubtaskProps[] | undefined;
+};
+
+function Card({ children }: CardProps) {
+  return (
+    <div className="border-solid border-2 border-sky-500 m-2 p-2 rounded">
+      {children}
+    </div>
+  );
+}
+
+function Task({ name, duration, subtasks }: TaskProps) {
+  let taskItem = null;
+
+  if (subtasks?.length) {
+    taskItem = (
+      <Card>
+        <li>
+          {name} ({duration})
+          <ul className="list-none pl-12">
+            {subtasks.map((subtask) => (
+              <Subtask name={subtask.name} duration={subtask.duration} />
+            ))}
+          </ul>
+        </li>
+      </Card>
+    );
+  } else {
+    taskItem = (
+      <Card>
+        <li>
+          {name} ({duration})
+        </li>
+      </Card>
+    );
+  }
+
+  return taskItem;
+}
+
+function Subtask({ name, duration }: SubtaskProps) {
+  return (
+    <li>
+      {name} ({duration})
+    </li>
+  );
+}
+
 export default function App() {
-  return <h1 className="uppercase">Hello world</h1>;
+  const tasks = [
+    {
+      name: "Task 1",
+      duration: 20,
+      subtasks: [
+        { name: "Subtask 1", duration: 5 },
+        { name: "Subtask 2", duration: 5 },
+        { name: "Subtask 3", duration: 10 },
+      ],
+    },
+    {
+      name: "Task 2",
+      duration: 25,
+    },
+  ];
+
+  return (
+    <>
+      <h1 className="text-center uppercase font-bold text-2xl">Tasks</h1>
+      <ul>
+        {tasks.map((task) => (
+          <Task
+            name={task.name}
+            duration={task.duration}
+            subtasks={task.subtasks}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
