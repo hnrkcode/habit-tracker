25b91d5aeb4c859f2f2095fc139f78383900211c
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-07-20 18:11:01 +0200

    Make modal scrollable if the content doesn't fit the screen height

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,7 +9,9 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
         className="absolute inset-0 bg-gray-800 opacity-75"
         onClick={onClose}
       ></div>
-      <div className="bg-white rounded-lg p-8 z-10">{children}</div>
+      <div className="bg-white rounded-lg p-8 z-10 max-h-[80vh] overflow-auto">
+        {children}
+      </div>
     </div>
   );
 }
