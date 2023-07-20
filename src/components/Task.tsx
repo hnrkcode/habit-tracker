3e09d70051ea3ab08cc225fc6098212472e0c3e2
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-07-20 21:23:14 +0200

    Refactor Task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -10,61 +10,41 @@ export default function Task({
   onSubtaskCheckbox,
 }: TaskProps) {
   const [showSubtasks, setShowSubtasks] = useState(false);
-  let taskItem = null;
 
   function handleToggleSubtasks() {
     setShowSubtasks(!showSubtasks);
   }
 
-  if (task.subtasks?.length) {
-    taskItem = (
-      <Card>
-        <li>
-          <TaskItem
-            id={task.id}
-            name={task.name}
-            duration={task.duration}
-            done={task.done}
-            showSubtasks={showSubtasks}
-            onToggle={handleToggleSubtasks}
-            onCheckbox={onTaskCheckbox}
-          />
-          <div>
-            {showSubtasks && (
-              <ul className="list-none pl-12">
-                {task.subtasks.map((subtask) => (
-                  <Subtask
-                    key={subtask.subtaskId}
-                    taskId={task.id}
-                    subtaskId={subtask.subtaskId}
-                    name={subtask.name}
-                    duration={subtask.duration}
-                    done={subtask.done}
-                    onSubtaskCheckbox={onSubtaskCheckbox}
-                  />
-                ))}
-              </ul>
-            )}
-          </div>
-        </li>
-      </Card>
-    );
-  } else {
-    taskItem = (
-      <Card>
-        <li>
-          <TaskItem
-            id={task.id}
-            name={task.name}
-            duration={task.duration}
-            done={task.done}
-            onToggle={handleToggleSubtasks}
-            onCheckbox={onTaskCheckbox}
-          />
-        </li>
-      </Card>
-    );
-  }
+  const subtasks = task.subtasks?.length && showSubtasks && (
+    <ul className="list-none pl-12">
+      {task.subtasks.map((subtask) => (
+        <Subtask
+          key={subtask.subtaskId}
+          taskId={task.id}
+          subtaskId={subtask.subtaskId}
+          name={subtask.name}
+          duration={subtask.duration}
+          done={subtask.done}
+          onSubtaskCheckbox={onSubtaskCheckbox}
+        />
+      ))}
+    </ul>
+  );
 
-  return taskItem;
+  return (
+    <Card>
+      <li>
+        <TaskItem
+          id={task.id}
+          name={task.name}
+          duration={task.duration}
+          done={task.done}
+          showSubtasks={task.subtasks ? showSubtasks : undefined}
+          onToggle={handleToggleSubtasks}
+          onCheckbox={onTaskCheckbox}
+        />
+        {subtasks}
+      </li>
+    </Card>
+  );
 }
