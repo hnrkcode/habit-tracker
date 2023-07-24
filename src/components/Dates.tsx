e55081e38ffba9bededfaf45a1477a9550a29468
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-07-24 21:02:52 +0200

    Refactor Dates component so the dates are scrollable

--- src/components/Dates.tsx
+++ src/components/Dates.tsx
@@ -1,32 +1,44 @@
-import { useState } from "react";
+import { useState, WheelEvent } from "react";
 import dayjs from "dayjs";
 
 export default function Dates() {
   const [selectedDateIndex, setSelectedDateIndex] = useState(3);
+  const [scrollPosition, setScrollPosition] = useState(0);
   const currentDate = dayjs();
-  const allDates = Array.from({ length: 7 }, (_, i) => ({
-    dayName: currentDate.add(i - 3, "day").format("ddd"),
-    date: currentDate.add(i - 3, "day").format("MMM D"),
-  }));
+  const datesArray = Array.from({ length: 7 }, (_, index) =>
+    currentDate.add(index + scrollPosition - 3, "day")
+  );
 
   function handleDateClick(index: number) {
     setSelectedDateIndex(index);
+    console.log("currently selected date:", datesArray[index - scrollPosition]);
+  }
+
+  function handleWheelScroll(event: WheelEvent) {
+    const newScrollPosition = scrollPosition - Math.sign(event.deltaY);
+    setScrollPosition(newScrollPosition);
   }
 
   return (
-    <div className="flex justify-between">
-      {allDates.map(({ dayName, date }, index) => (
+    <div
+      className="flex justify-between overflow-hidden"
+      onWheel={handleWheelScroll}
+    >
+      {datesArray.map((date, index) => (
         <div
           className={`select-none cursor-pointer p-1 m-1 ${
-            selectedDateIndex === index
-              ? "border-blue-500 border-2 rounded-lg"
+            selectedDateIndex === index + scrollPosition
+              ? "border-sky-500 border-2 rounded-lg"
               : ""
           }`}
-          key={index}
-          onClick={() => handleDateClick(index)}
+          key={index + scrollPosition}
+          onClick={() => handleDateClick(index + scrollPosition)}
         >
-          <div className="text-center text-lg font-bold">{dayName}</div>
-          <div className="text-center">{date}</div>
+          <div className="text-center text-lg font-bold">
+            {date.format("ddd")}
+          </div>
+          <div className="text-center">{date.format("MMM")}</div>
+          <div className="text-center">{date.format("D")}</div>
         </div>
       ))}
     </div>
