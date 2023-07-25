e830b7cef615185789c9d9fef84063e8f1ae8e78
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-07-25 23:29:43 +0200

    Refactor Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,17 +1,17 @@
 import { ModalProps } from "../types/common";
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div
-        className="absolute inset-0 bg-gray-800 opacity-75"
-        onClick={onClose}
-      ></div>
-      <div className="bg-white rounded-lg p-8 z-10 max-h-[80vh] overflow-auto">
-        {children}
+    isOpen && (
+      <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div
+          className="absolute inset-0 bg-gray-800 opacity-75"
+          onClick={onClose}
+        ></div>
+        <div className="bg-white rounded-lg p-8 z-10 max-h-[80vh] overflow-auto">
+          {children}
+        </div>
       </div>
-    </div>
+    )
   );
 }
